41b3e71fcacce5283037db0b1579c7fa3bec736b
JihyunHan <gkswlgus923@gamil.com>
2024-07-15 22:02:08 +0900

    2096. 내려가기

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,38 @@
 import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc  = new Scanner(System.in);
-        int n = sc.nextInt(); // 1보다 크고 100보다 작다
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] max = new int[n+2];
+        int[] min = new int[n+2];
+        int[] tmax = new int[n+2];
+        int[] tmin = new int[n+2];
+        int input = 0;
 
-       int[][] dp = new int[n+1][10];
+        max[0] = max[4] = 0;
+        min[0] = min[4] = 1_000_000;
 
-       for(int i = 1; i <= 9; i++) {
-        dp[1][i] = 1;
-       }
-
-       for(int i = 2; i <= n; i++) {
-        for(int l = 0; l <= 9; l++) {
-            if (l == 0) {
-                dp[i][0] = dp[i-1][l+1] % 1000_000_000;
-            }else if(l == 9) {
-                dp[i][9] = dp[i-1][l-1] % 1000_000_000;
-            }else {
-                dp[i][l] = (dp[i-1][l-1] + dp[i-1][l+1]) % 1000_000_000;
+        
+        for (int i = 1; i <= n; i++) {
+            for(int j = 1; j <= 3; j++) {
+                input = sc.nextInt();
+                tmax[j] = Math.max(max[j-1], Math.max(max[j], max[j+1])) + input;
+                tmin[j]= Math.min(min[j-1], Math.min(min[j], min[j+1])) + input;
             }
+            for(int k = 1; k <= 3; k++) {
+                max[k] = tmax[k];
+                min[k] = tmin[k];
+            }
+
         }
-       }
-       long sum = 0;
-       for(int i = 0; i <= 9; i++) {
-            sum = (sum +dp[n][i]) % 1000_000_000;
-       }
-       System.out.println(sum);
+
+        int maxVal = 0;
+        int minVal = 1_000_000;
+        for(int i = 1; i <= 3; i++) {
+            maxVal = Math.max(maxVal, max[i]);
+            minVal = Math.min(minVal, min[i]);
+        }
+
+        System.out.println(maxVal +" "+minVal);
     }
-}
+    }
